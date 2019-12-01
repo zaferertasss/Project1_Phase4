c911d6934f2fd5c3ca83e478e6fb61cff8e03275
zaferertasss <57995676+zaferertasss@users.noreply.github.com>
2019-12-02 02:59:58 +0300

    Add files via upload

---
+++ Phase1_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    system("cd");
+    system("dir");
+
+    system("cd C:\\Users\\Desktop && dir");
+    system("dir");
+    system("pause");
+    return 0;
+}
